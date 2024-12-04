bbf46f435dc4f254caa7af0d6b7a600f179051a1
Vincent   Huynh <huynhv@power.arc.losrios.edu>
2024-12-04 08:20:01 -0800

    vincent-particle+matrices-nottested

--- code/Particle.cpp
+++ code/Particle.cpp
@@ -1,5 +1,7 @@
 #include "Particle.h"
 
+srand(time(0));
+
 bool Particle::almostEqual(double a, double b, double eps)
 {
 	return fabs(a - b) < eps;
@@ -144,7 +146,80 @@ void Particle::unitTests()
     cout << "Score: " << score << " / 7" << endl;
 }
 
-Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosition)
+void Particle::draw(RenderTarget& target, RenderStates states) const
 {
+    sf::VertexArray lines(sf::TriangleFan, m_numPoints + 1);
+    Vector2f center = target.mapCoordsToPixel(m_centerCoordinate, m_cartesianPlane);
+    lines[0].position = center;
+    lines[0].color = m_color1;
+    for (int j  = 1; j <= m_numPoints; j++)
+    {
+        lines[j].position = target.mapCoordsToPixel(Vector2f(m_A(0, j-1)), m_cartesianPlane);
+        lines[j].color = m_Color2;
+    }
+    target.draw(lines);
+}
 
+void Particle::update(float dt)
+{
+    m_ttl -= dt;
+    rotate(dt * m_radiansPerSec);
+    scale(SCALE);
+    float dx = m_vx * dt;
+    m_vy -= G * dt;
+    float dy = m_vy * dt;
+    translate(dx, dy);
+}
+
+Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosition) : m_A(2, numPoints)
+{
+    m_ttl = TTL;
+    m_numPoints = numPoints;
+    m_radiansPerSec = (float)rand() / (RAND_MAX) * M_PI;
+    m_cartesianPlane.setCenter(0,0);
+    m_cartesianPlane.setSize(target.getSize().x, (-1.0) * target.getSize().y);
+    m_centerCoordinate = target.mapPixelToCoords(mouseClickPosition, m_cartesianPlane);
+    m_vx = 100 + ((float)rand() / (RAND_MAX) * 400);
+    m_vy = 100 + ((float)rand() / (RAND_MAX) * 400);
+    m_color1 = Color(255, 255, 255);
+    m_color2 = Color(rand() % 256, rand() % 256, rand() % 256);
+
+    // The algorithm
+    float theta = (float)rand() / (RAND_MAX) * M_PI;
+    float dTheta = 2 * M_PI / (numPoints - 1);
+    for (int j = 0; i < numPoints; i++)
+    {
+        float r = 20 + ((float)rand() / (RAND_MAX) * 60)
+        float dx = r * cos(theta);
+        float dy = r * sin(theta);
+        m_A(0, j) = m_centerCoordinate.x + dx;
+        m_A(1, j) = m_centerCoordinate.y + dy;
+        theta += dTheta;
+    }
+}
+
+void Particle::translate(double xShift, double yShift)
+{
+    TranslationMatrix T(xShift, yShift);
+    m_A = T + m_A;
+    m_centerCoordinate.x += xShift;
+    m_centerCoordinate.y += yShift;
+}
+
+void Particle::rotate(double theta)
+{
+    Vector2f temp = m_centerCoordinate;
+    translate(-m_centerCoordinate.x, -m_centerCoordinate.y);
+    RotationMatrix R(theta);
+    m_A = R * m_A;
+    translate(temp.x, temp.y);
+}
+
+void Particle::scale(double c)
+{
+    Vector2f temp = m_centerCoordinate;
+    translate(-m_centerCoordinate.x, -m_centerCoordinate.y);
+    ScalingMatrix S(c);
+    m_A = S * m_A;
+    translate(temp.x, temp.y);
 }
